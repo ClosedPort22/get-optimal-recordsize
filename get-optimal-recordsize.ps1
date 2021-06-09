ddcf4ea4c1bd38c52e5fbbcdbae969f10e638105
coderoctopus <coderoctopus@foxmail.com>
2021-06-09 19:15:51 +0800

    Cleanup

--- get-optimal-recordsize.ps1
+++ get-optimal-recordsize.ps1
@@ -1,27 +1,20 @@
 param (
 	[int]$MaxPower=20, # 1m
 	[int]$ashift=12,
-	[string]$Path='.',
-	[int]$AvgMetadataPerBlock=97 # Empirical value for COMPRESSED metadata
+	[ValidateScript({Test-Path -LiteralPath $_}, ErrorMessage="The specified directory does not exist.")][string]$Path='.',
+	[int]$AvgMetadataPerBlock=97 # Empirical value for compressed metadata
 )
-
 $MinPower=12 # 4k
 $SectorSize=[Math]::Pow(2,$ashift)
-$List=gci -File -Path $Path -Recurse
-$DataSize=0
-foreach($Item in $List){
-	$DataSize+=$Item.Length
-}
+$FileList=gci -File -Path $Path -Recurse
+$DataSize=(gci -path $Path -File -Recurse|measure -Sum Length).Sum
 Write-Host "Data size is $DataSize bytes"
 for($i=$MinPower;$i -le $MaxPower;++$i){
 	$Current=[Math]::Pow(2,$i)
 	$ActualSize=0
-	foreach($Item in $List){
-		if($Item.Length -ge $Current){
-			$ActualSize+=([Math]::Ceiling($Item.Length/$Current)*($Current+$AvgMetadataPerBlock))
-			continue
-		}
-		$ActualSize+=([Math]::Ceiling($Item.Length/$SectorSize)*($SectorSize+$AvgMetadataPerBlock))
+	foreach($Item in $FileList){
+		$Unit=if($Item.Length -ge $Current){$Current}else{$SectorSize}
+		$ActualSize+=[Math]::Ceiling($Item.Length/$Unit)*($Unit+$AvgMetadataPerBlock)
 	}
 	$Overhead=($ActualSize-$DataSize)/$DataSize
 	New-Object psobject -Property ([ordered]@{'Record Size'=$Current;'Actual Size'=$ActualSize;'Slack Space'=$ActualSize-$DataSize;'Slack Space %'=[String]::Format('{0:P2}', $Overhead)})
