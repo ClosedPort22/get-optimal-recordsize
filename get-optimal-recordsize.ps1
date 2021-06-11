141d6d587b3dea4f84b4f8c908de3fb099e06d46
coderoctopus <coderoctopus@foxmail.com>
2021-06-11 19:48:12 +0800

    Merged raidz calculator into get-optimal-recordsize.ps1

--- get-optimal-recordsize.ps1
+++ get-optimal-recordsize.ps1
@@ -1,21 +1,38 @@
 param (
+	[Parameter(Mandatory)][ValidateRange(0,3)][int]$RaidLevel, # specify 0 for striped or mirrored pools
+	[Parameter(Mandatory)][int]$StripeWidth, # =the number of disks in each vdev(?)
 	[int]$MaxPower=20, # 1m
 	[int]$ashift=12,
 	[ValidateScript({Test-Path -LiteralPath $_}, ErrorMessage="The specified directory does not exist.")][string]$Path='.',
 	[int]$AvgMetadataPerBlock=97 # Empirical value for compressed metadata
 )
 $MinPower=12 # 4k
+$MinStripeSize=$RaidLevel+1
 $SectorSize=[Math]::Pow(2,$ashift)
 $FileList=gci -File -Path $Path -Recurse
-$DataSize=(gci -path $Path -File -Recurse|measure -Sum Length).Sum
+$DataSize=($FileList|measure -Sum Length).Sum
 Write-Host "Data size is $DataSize bytes"
 for($i=$MinPower;$i -le $MaxPower;++$i){
-	$Current=[Math]::Pow(2,$i)
+	$CurrentRecordSize=[Math]::Pow(2,$i)
 	$ActualSize=0
 	foreach($Item in $FileList){
-		$Unit=if($Item.Length -ge $Current){$Current}else{$SectorSize}
-		$ActualSize+=[Math]::Ceiling($Item.Length/$Unit)*($Unit+$AvgMetadataPerBlock)
+		#Write-Verbose ($Item|Format-Table Name,Length|Out-String)
+		if($Item.Length -ge $CurrentRecordSize){
+			$DataBlockCount=[Math]::Ceiling($Item.Length/$CurrentRecordSize)
+			$BlockSize=$CurrentRecordSize
+		}else{
+			$DataBlockCount=1
+			$BlockSize=[Math]::Ceiling($Item.Length/$SectorSize)*$SectorSize
+		}
+		#Write-Verbose "This file has $DataBlockCount block(s)"
+		$SectorsPerBlock=$BlockSize/$SectorSize
+		$Parity=[Math]::Ceiling($SectorsPerBlock/($StripeWidth-$RaidLevel))*$RaidLevel
+		$BloatedSectorCount=[Math]::Ceiling(($SectorsPerBlock+$Parity)/$MinStripeSize)*$MinStripeSize
+		#Write-Verbose "Each block has $Parity parity sectors, occupies $BloatedSectorCount sectors in total"
+		$Theoretical=$SectorsPerBlock*$StripeWidth/($StripeWidth-$RaidLevel)
+		#Write-Verbose "Each block ideally occupies $Theoretical sectors"
+		$ActualSize+=[Math]::Ceiling($DataBlockCount*($BlockSize*$BloatedSectorCount/$Theoretical+$AvgMetadataPerBlock))
 	}
-	$Overhead=($ActualSize-$DataSize)/$DataSize
-	New-Object psobject -Property ([ordered]@{'Record Size'=$Current;'Actual Size'=$ActualSize;'Slack Space'=$ActualSize-$DataSize;'Slack Space %'=[String]::Format('{0:P2}', $Overhead)})
+	$Overhead=$ActualSize/$DataSize-1
+	New-Object psobject -Property ([ordered]@{'Record Size'=$CurrentRecordSize;'Actual Size'=$ActualSize;'Overhead %'=[String]::Format('{0:P2}', $Overhead)})
 }
