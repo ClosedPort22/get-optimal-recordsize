21f6eba2bce3f728c3bf4fbe212595828f247497
coderoctopus <coderoctopus@foxmail.com>
2021-06-11 21:04:10 +0800

    Added readme

--- get-optimal-recordsize.ps1
+++ get-optimal-recordsize.ps1
@@ -1,18 +1,18 @@
 param (
-	[Parameter(Mandatory)][ValidateRange(0,3)][int]$RaidLevel, # specify 0 for striped or mirrored pools
+	[Parameter(Mandatory)][ValidateRange(0,3)][int]$RAIDZLevel, # specify 0 for striped or mirrored pools
 	[Parameter(Mandatory)][int]$StripeWidth, # =the number of disks in each vdev(?)
-	[int]$MaxPower=20, # 1m
+	[int]$MaxExponent=20, # 1m
 	[int]$ashift=12,
 	[ValidateScript({Test-Path -LiteralPath $_}, ErrorMessage="The specified directory does not exist.")][string]$Path='.',
 	[int]$AvgMetadataPerBlock=97 # Empirical value for compressed metadata
 )
 $MinPower=12 # 4k
-$MinStripeSize=$RaidLevel+1
+$MinStripeSize=$RAIDZLevel+1
 $SectorSize=[Math]::Pow(2,$ashift)
 $FileList=gci -File -Path $Path -Recurse
 $DataSize=($FileList|measure -Sum Length).Sum
 Write-Host "Data size is $DataSize bytes"
-for($i=$MinPower;$i -le $MaxPower;++$i){
+for($i=$MinPower;$i -le $MaxExponent;++$i){
 	$CurrentRecordSize=[Math]::Pow(2,$i)
 	$ActualSize=0
 	foreach($Item in $FileList){
@@ -26,10 +26,10 @@ for($i=$MinPower;$i -le $MaxPower;++$i){
 		}
 		#Write-Verbose "This file has $DataBlockCount block(s)"
 		$SectorsPerBlock=$BlockSize/$SectorSize
-		$Parity=[Math]::Ceiling($SectorsPerBlock/($StripeWidth-$RaidLevel))*$RaidLevel
+		$Parity=[Math]::Ceiling($SectorsPerBlock/($StripeWidth-$RAIDZLevel))*$RAIDZLevel
 		$BloatedSectorCount=[Math]::Ceiling(($SectorsPerBlock+$Parity)/$MinStripeSize)*$MinStripeSize
 		#Write-Verbose "Each block has $Parity parity sectors, occupies $BloatedSectorCount sectors in total"
-		$Theoretical=$SectorsPerBlock*$StripeWidth/($StripeWidth-$RaidLevel)
+		$Theoretical=$SectorsPerBlock*$StripeWidth/($StripeWidth-$RAIDZLevel)
 		#Write-Verbose "Each block ideally occupies $Theoretical sectors"
 		$ActualSize+=[Math]::Ceiling($DataBlockCount*($BlockSize*$BloatedSectorCount/$Theoretical+$AvgMetadataPerBlock))
 	}
